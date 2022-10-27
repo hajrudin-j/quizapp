92fb24ceac00ed899bb2da09b86a67a5673e9086
Hajrudin J <hajrudin.jupic@gmail.com>
2022-10-27 15:01:37 +0200

    new commit - day & night

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  nightVariable: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
